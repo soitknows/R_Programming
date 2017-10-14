adb169c3b5ce850c9f3930cc62e020db9de1015a
soitknows <and.tarver@gmail.com>
2017-10-13 19:56:08 -0700

    modified read.csv to change Not Available factors to NAs

--- ProgrammingAssignment3/hospital-outcomes.R
+++ ProgrammingAssignment3/hospital-outcomes.R
@@ -1,6 +1,6 @@
 best <- function (state = "CA", type = "pneumonia") {
   
-  outcomes <- read.csv("outcome-of-care-measures.csv")
+  outcomes <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available")
   outcomes <- outcomes[,c(2,7,11,17,23)]
   colnames(outcomes) <- c("Hospital.Name",
                          "State",
@@ -8,12 +8,13 @@ best <- function (state = "CA", type = "pneumonia") {
                          "heart failure",
                          "pneumonia")
   hospitals <- outcomes[outcomes$State == state 
-                        & outcomes[type] != "Not Available",
+                        & !is.na(outcomes[type]),
                         c(type,"Hospital.Name")]
   
-  hospitals[type] <- lapply(hospitals[type], as.numeric)
-  
+  hospitals[type] <- sapply(hospitals[type], as.numeric)
   best_hosps <- sort(as.vector(hospitals[hospitals[type] == min(hospitals[type]),2]))
   best_hosps[1]
   
 }
+
+
