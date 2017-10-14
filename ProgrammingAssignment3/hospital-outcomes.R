2f49a0a8ab5288bd0613558e34c3af1eacbabe35
soitknows <and.tarver@gmail.com>
2017-10-14 13:28:46 -0700

    added error handling for func arguments

--- ProgrammingAssignment3/hospital-outcomes.R
+++ ProgrammingAssignment3/hospital-outcomes.R
@@ -1,18 +1,20 @@
-best <- function (state = "CA", type = "pneumonia") {
+best <- function (state = "CA", outcome = "pneumonia") {
   
   outcomes <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available")
   outcomes <- outcomes[,c(2,7,11,17,23)]
-  colnames(outcomes) <- c("Hospital.Name",
-                         "State",
-                         "heart attack",
-                         "heart failure",
-                         "pneumonia")
-  hospitals <- outcomes[outcomes$State == state 
-                        & !is.na(outcomes[type]),
-                        c(type,"Hospital.Name")]
+  cols <- c("Hospital.Name", "State","heart attack","heart failure","pneumonia")
+  colnames(outcomes) <- cols
+  states <- unique(outcomes$State)
   
-  hospitals[type] <- sapply(hospitals[type], as.numeric)
-  best_hosps <- sort(as.vector(hospitals[hospitals[type] == min(hospitals[type]),2]))
+  if ( !outcome %in% cols) stop("invalid outcome")
+  if ( !state %in% states) stop("invalid state")
+  
+  hospitals <- outcomes[outcomes$State == state 
+                        & !is.na(outcomes[outcome]),
+                        c(outcome,"Hospital.Name")]
+  
+  hospitals[outcome] <- sapply(hospitals[outcome], as.numeric)
+  best_hosps <- sort(as.vector(hospitals[hospitals[outcome] == min(hospitals[outcome]),2]))
   best_hosps[1]
   
 }
